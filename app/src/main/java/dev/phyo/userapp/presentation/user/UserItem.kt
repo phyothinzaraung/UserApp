4d5d69490442d708d6fc15ca994d7b7f45bddb3e
Phyo Thinzar Aung <phyothinzar.ag92@gmail.com>
2025-01-27 09:35:52 -0600

    - UI touch up commit

--- app/src/main/java/dev/phyo/userapp/presentation/user/UserItem.kt
+++ app/src/main/java/dev/phyo/userapp/presentation/user/UserItem.kt
@@ -2,10 +2,13 @@ package dev.phyo.userapp.presentation.user
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
@@ -19,9 +22,11 @@ fun UserItem(user: User, onClick: () -> Unit) {
         modifier = Modifier
             .fillMaxWidth()
             .clickable(onClick = onClick)
-            .padding(16.dp)
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         GlideImage(model = user.avatar, contentDescription = "user image")
+        Spacer(modifier = Modifier.width(8.dp))
         Text(user.email)
     }
 }
--- app/src/main/java/dev/phyo/userapp/presentation/user/UserList.kt
+++ app/src/main/java/dev/phyo/userapp/presentation/user/UserList.kt
@@ -1,20 +1,25 @@
 package dev.phyo.userapp.presentation.user
 
 import android.util.Log
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import dev.phyo.userapp.data.remote.model.User
 
 @Composable
-fun Userlist(users: List<User>, navController: NavHostController) {
-    LazyColumn {
-        items(users){ user ->
-            UserItem(user, onClick = {
-                navController.navigate("user_details/${user.id}")
-                Log.d("UserId", user.id.toString())
-            })
+fun Userlist(users: List<User>, navController: NavHostController, modifier: Modifier = Modifier) {
+    Scaffold { innerPadding ->
+        LazyColumn(modifier = modifier.padding(innerPadding)) {
+            items(users){ user ->
+                UserItem(user){
+                    navController.navigate("user_details/${user.id}")
+                }
+            }
         }
     }
+
 }
